66149016b5c51370704ba92052c35081f81d39e8
declan1397 <declan.gon@gmail.com>
2017-10-15 19:08:55 -0700

    Added product page

--- custom.js
+++ custom.js
@@ -15,4 +15,27 @@ $(document).ready(function() {
     $('.criteriaRolloverPalette').hover(function () {
         $(this).css('display', 'none');
     });
+
+    $('.consumerSelector').click(function () {
+        $(this).removeClass("selectorSelected").addClass("selectorSelected");
+        $('.hpSelector').removeClass("selectorSelected");
+        $('.consumerBanner').css('display', 'flex');
+        $(this).css('padding-top', '14px');
+        $('.hpBanner').css('display', 'none');
+    });
+
+    $('.hpSelector').click(function () {
+        $(this).removeClass("selectorSelected").addClass("selectorSelected");
+        $('.consumerSelector').removeClass("selectorSelected");
+        $('.consumerBanner').css('display', 'none');
+        $('.hpBanner').css('display', 'flex');
+        $('.consumerSelector').css('padding-top', '12px');
+    });
+
+
+
+
+
+
+
 });
